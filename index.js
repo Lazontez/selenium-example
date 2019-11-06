41323dd450a2e8f7b51631539f7f913caaab4336
Tez, Gardner <tezgardner1212@gmail.com>
2019-11-06 09:00:52 -0600

    messing around with changing proxy settings in chrome

--- index.js
+++ index.js
@@ -1,14 +1,37 @@
-const {Builder,By,Key,util} = require("selenium-webdriver");
-async function example(){
-    
-    let driver = await new Builder().forBrowser("chrome").build();
-    await driver.get("https://google.com").catch((err)=>{
-        console.log("error with connecting to google")
-        console.log(err)
-    });
-    await driver.findElement(By.name("q")).sendKeys("Lazontez Gardner", Key.ENTER).catch((err)=>{
-        console.log("error with serching for selenium")
-        console.log(err)
-    })
-}
-example()
+//--------------- EXAMPLE 1 
+
+// const {Builder,By,Key,util} = require("selenium-webdriver");
+
+// async function example(){
+
+//     let driver = await new Builder().forBrowser("chrome").build();
+//     await driver.get("https://google.com").then(res=>{
+//         console.log(res)
+//     }).catch((err)=>{
+//         console.log("error with connecting to google")
+//         console.log(err)
+//     });
+//     await driver.findElement(By.name("q")).sendKeys("Lazontez Gardner", Key.ENTER).catch((err)=>{
+//         console.log("error with serching for selenium")
+//         console.log(err)
+//     })
+// }
+// example()
+
+// ----------------EXAMPLE 2
+
+const { Builder } = require("selenium-webdriver");
+const proxy = require("selenium-webdriver/proxy");
+const proxyServer = "172.58.84.244"
+
+const driver = new Builder(
+    proxy.manual(
+        {
+          http: proxyServer,
+          https: proxyServer
+        }
+    )
+).forBrowser("chrome").build();
+
+driver.get("https://vital-town.herokuapp.com/")
+
