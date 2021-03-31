cb7fcd7cc199e81f084508f44a0095a9a73c6e83
Tez, Gardner <tezgardner1212@gmail.com>
2021-03-30 21:58:09 -0500

    Twitter Sign In

--- index.js
+++ index.js
@@ -1,29 +1,14 @@
-//--------------- EXAMPLE 1 
-
-// const {Builder,By,Key,util} = require("selenium-webdriver");
-
-// async function example(){
-
-//     let driver = await new Builder().forBrowser("chrome").build();
-//     await driver.get("https://google.com").then(res=>{
-//         console.log(res)
-//     }).catch((err)=>{
-//         console.log("error with connecting to google")
-//         console.log(err)
-//     });
-//     await driver.findElement(By.name("q")).sendKeys("Lazontez Gardner", Key.ENTER).catch((err)=>{
-//         console.log("error with serching for selenium")
-//         console.log(err)
-//     })
-// }
-// example()
-// ----------------EXAMPLE 2
-
+//--------------- Enter Fields For Twitter Sign In Pop 1 
 
 const { Builder, By, Key, until } = require("selenium-webdriver");
-const chrome = require("selenium-webdriver/chrome");
-const testWord =  "br"
 
+const chrome = require("selenium-webdriver/chrome");
+const signInData = {
+    'username' : "Joshua" ,
+    "phonenumber": "6154753959" ,
+    "Date":['Dec' , '12' , '1999']
+
+}
 
 const options = new chrome.Options({ args: ['--window-size=1280,800', '--auto-open-devtools-for-tabs'] })
 
@@ -36,42 +21,25 @@ const proxy = require("selenium-webdriver/proxy");
 // )
 
 
-async function example2() {
+async function twitterSignIn() {
     const driver = new Builder()
         .setChromeOptions({ options: options })
         .forBrowser("chrome").build();
-    await driver.get("http://vital-town.herokuapp.com/authentication/signup");
 
-    driver.wait(until.elementLocated(By.id("businessnameInput"))).then(res => {
+       await driver.get("https://twitter.com/i/flow/signup");
 
-        console.log("Business name input field has been rendered")
-        driver.findElement(By.name("accountUsername")).sendKeys(testWord).then(res => { console.log("Account Username has been entered ") }).catch(err => { console.log(err) });
-        driver.findElement(By.name("accountPw")).sendKeys(testWord).then(res => { console.log("Account password has been entered") }).catch(err => { console.log(err) });
-        driver.findElement(By.name("accountPwConfirm")).sendKeys(testWord).then(res => { console.log("Accouny Password was confirmed") })
-        driver.findElement(By.id("signUpButton")).sendKeys(Key.ENTER)
+    driver.wait(until.elementLocated(By.name("name"))).then(res => {
+
+        console.log("Starting to fill out log-in page")
+        driver.findElement(By.name("name")).sendKeys(signInData.username).then(res => { console.log(`Account Username has been entered as ${signInData.username}`) }).catch(err => { console.log(err) });
+        driver.findElement(By.name("phone_number")).sendKeys(signInData.phonenumber).then(res => { console.log("Account password has been entered") }).catch(err => { console.log(err) });
+        driver.findElement(By.id("Month")).sendKeys(signInData.Date[0]).then(res => { console.log("Month has been entered") })
+        driver.findElement(By.id("Day")).sendKeys(signInData.Date[1]).then(res => { console.log("Day has been entered") })
+        driver.findElement(By.id("Year")).sendKeys(signInData.Date[2]).then(res => { console.log("Year has been entered") 
+    })       
     })
-    driver.wait(until.elementLocated(By.name("loginPw"))).then(res => {
-
-        console.log("Login Page Rendered")
-        driver.findElement(By.name("loginName")).sendKeys(testWord).then(res => { console.log("Account Username has been entered") }).catch(err => { console.log(err) });
-        driver.findElement(By.name("loginPw")).sendKeys(testWord).then(res => { console.log("Login Password password has been entered") }).catch(err => { console.log(err) });
-        driver.findElement(By.id("loginBtn")).sendKeys(Key.ENTER)
-    })
-    // driver.wait(until.elementLocated(By.name("loginPw"))).then(res => {
-
-    //     console.log("Login Page Rendered")
-    //     driver.findElement(By.name("loginName")).sendKeys(testWord).then(res => { console.log("Account Username has been entered") }).catch(err => { console.log(err) });
-    //     driver.findElement(By.name("loginPw")).sendKeys(testWord).then(res => { console.log("Login Password password has been entered") }).catch(err => { console.log(err) });
-    //     driver.findElement(By.id("loginBtn")).sendKeys(Key.ENTER)
-    // })
-
-
-}
-if(testWord===null){
-    console.error("Test word equals " + typeof(testWord))
-}
-else{
-    example2()
+    
 
 }
 
+twitterSignIn()
